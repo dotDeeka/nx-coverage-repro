bcbc0045f0e5b9d46e65ab57e4c60ac4eab89932
Derek Pavao <dpavao@intersections.com>
2020-02-26 08:38:45 -0500

    initial-commit

---
+++ apps/example-app/__tests__/index.spec.ts
@@ -0,0 +1,7 @@
+import { fnToTest } from '../src/app';
+
+describe('fnToTest', () => {
+  it('should return true', () => {
+    expect(fnToTest()).toBe(true);
+  });
+});
---
+++ apps/example-app/src/app/index.ts
@@ -0,0 +1,3 @@
+export function fnToTest(): boolean {
+  return true;
+}
